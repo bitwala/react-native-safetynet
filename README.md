98a10fbf046980eaea62786e872fc652b286e5e7
Léo Pradel <pradel.leo@gmail.com>
2021-01-12 12:57:12 +0100

    feat: first version of the package (#3)

--- README.md
+++ README.md
@@ -1,3 +1,75 @@
-# react-native-safetynet
+# Google SafetyNet Attestation for React Native
 
-Google SafetyNet Attestation implementation for React Native
+## About SafetyNet
+
+Google provides an API to verify device integrity and detect harmful apps. See the [SafetyNet documentation](https://developer.android.com/training/safetynet/index.html) for more information.
+
+## Getting Started
+
+`$ npm install @bitwala/react-native-safetynet`
+
+or
+
+`$ yarn add react-native-google-safetynet-client`
+
+### Mostly Automatic Installation
+
+`$ react-native link @bitwala/react-native-safetynet`
+
+### Manual installation
+
+#### Android
+
+1. Open up `android/app/src/main/java/[...]/MainActivity.java`
+
+- Add `import com.bitwala.safetynet.RNSafetyNetPackage;` to the imports at the top of the file
+- Add `new RNSafetyNetPackage()` to the list returned by the `getPackages()` method
+
+2. Append the following lines to `android/settings.gradle`:
+   ```
+   include ':@bitwala/react-native-safetynet'
+   project(':@bitwala/react-native-safetynet').projectDir = new File(rootProject.projectDir, 	'../node_modules/@bitwala/react-native-safetynet/android')
+   ```
+3. Insert the following lines inside the dependencies block in `android/app/build.gradle`:
+   ```
+     implementation project(':@bitwala/react-native-safetynet')
+   ```
+
+## Usage
+
+```javascript
+import RNSafetyNetClient from '@bitwala/react-native-safetynet';
+
+// STEP 1: Send nonce request to server with device unique id
+const deviceId =
+  'You can use the "react-native-device-info" module to get the unique id of device';
+
+const nonceResult = await RNSafetyNetClient.requestNonce({
+  endPointUrl: 'https://your-server/nonce-creator-endpoint',
+  additionalData: deviceId,
+});
+
+if (!nonceResult.nonce || nonceResult.error) {
+  console.log(nonceResult.error);
+  return;
+}
+
+// STEP 2: Send attestation request to Google Play
+
+const attestationResult = await RNSafetyNetClient.sendAttestationRequest(
+  nonceResult.nonce,
+  'YOUR API KEY'
+);
+
+if (!attestationResult.jws || attestationResult.error) {
+  console.log('Attestation request failed.');
+  return;
+}
+
+// STEP 3: Pass the JSW to your server for verification.
+
+const verification = await RNSafetyNetClient.verifyAttestationResult({
+  endPointUrl: 'https://your-server/verification-endpoint',
+  attestationJws: attestationResult.jws,
+});
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,136 @@
-export const hi = () => console.log('Hello');
+import fetch from 'node-fetch';
+import { NativeModules } from 'react-native';
+
+/**
+ * For more complex nonce you can pass additional string to server
+ */
+interface NonceRequestArgs {
+  endPointUrl: string;
+  additionalData?: string;
+}
+
+interface VerifyAttestationArgs {
+  endPointUrl: string;
+  attestationJws: string;
+}
+
+interface VerifyReCaptchaArgs {
+  endPointUrl: string;
+  token: string;
+}
+
+interface NonceRequestResult {
+  nonce: string | null;
+  error?: string;
+}
+
+interface AttestationResult {
+  jws: string | null;
+  error?: string;
+}
+
+interface ReCaptchaResult {
+  token: string | null;
+  error?: string;
+}
+
+type RequestNonce = (args: NonceRequestArgs) => Promise<NonceRequestResult>;
+type AttestationRequest = (
+  nonce: string,
+  apiKey: string
+) => Promise<AttestationResult>;
+type VerifyAttestation = (args: VerifyAttestationArgs) => Promise<any>;
+type ReCaptchaRequest = (apiKey: string) => Promise<ReCaptchaResult>;
+type VerifyReCaptcha = (args: VerifyReCaptchaArgs) => Promise<any>;
+
+const { RNSafetyNet } = NativeModules;
+
+const requestNonce: RequestNonce = async (args) =>
+  fetch(args.endPointUrl, {
+    body: JSON.stringify({ data: args.additionalData || '' }),
+    compress: false,
+    method: 'POST',
+  }).then((response) => {
+    if (response.status !== 200) {
+      return { error: response.statusText };
+    }
+
+    return response.json();
+  });
+
+const sendAttestationRequest: AttestationRequest = async (nonce, apiKey) => {
+  const attestationResult: AttestationResult = { jws: null };
+
+  const isPlayServicesAvailable: boolean = await RNSafetyNet.isPlayServicesAvailable();
+  if (!isPlayServicesAvailable) {
+    attestationResult.error = 'Google Play Service is not available!';
+  }
+
+  const atteResponse = await RNSafetyNet.sendAttestationRequest(
+    nonce,
+    apiKey
+  ).then((result: any) => result);
+
+  if (atteResponse) {
+    attestationResult.jws = atteResponse;
+  } else {
+    attestationResult.error = 'Attestation request failed!';
+  }
+
+  return attestationResult;
+};
+
+const verifyAttestationResult: VerifyAttestation = async (args) =>
+  fetch(args.endPointUrl, {
+    body: JSON.stringify({ data: args.attestationJws || '' }),
+    compress: false,
+    method: 'POST',
+  }).then((response) => {
+    if (response.status !== 200) {
+      return { error: response.statusText };
+    }
+
+    return response.json();
+  });
+
+const sendRecaptchaRequest: ReCaptchaRequest = async (apiKey) => {
+  const recaptchaResult: ReCaptchaResult = { token: null };
+
+  const isPlayServicesAvailable: boolean = await RNSafetyNet.isPlayServicesAvailable();
+  if (!isPlayServicesAvailable) {
+    recaptchaResult.error = 'Google Play Service is not available!';
+  }
+
+  const recaptchaResponse = await RNSafetyNet.verifyWithRecaptcha(apiKey).then(
+    (result: any) => result
+  );
+
+  if (recaptchaResponse) {
+    recaptchaResult.token = recaptchaResponse;
+  } else {
+    recaptchaResult.error = 'reCaptcha request failed!';
+  }
+
+  return recaptchaResult;
+};
+
+const verifyReCaptchaResult: VerifyReCaptcha = async (args) =>
+  fetch(args.endPointUrl, {
+    body: JSON.stringify({ data: args.token || '' }),
+    compress: false,
+    method: 'POST',
+  }).then((response) => {
+    if (response.status !== 200) {
+      return { error: response.statusText };
+    }
+
+    return response.json();
+  });
+
+export default {
+  requestNonce,
+  sendAttestationRequest,
+  verifyAttestationResult,
+  sendRecaptchaRequest,
+  verifyReCaptchaResult,
+};
